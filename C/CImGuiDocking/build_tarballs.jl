668788074833b5fb9d02388b1af6f41c4a390c73
Will Thompson <will.thompson@outlook.com>
2021-03-22 00:24:23 -0700

    Add version of CImGui that targets the docking branch at the same version, v0.79

---
+++ C/CImGuiDocking/build_tarballs.jl
@@ -0,0 +1,48 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "CImGuiDocking"
+version = v"1.79.0"
+
+# Collection of sources required to build CImGui
+sources = [
+    GitSource("https://github.com/ocornut/imgui.git",
+              "682249396f02b8c21e5ff333ab4a1969c89387ad"),
+
+    GitSource("https://github.com/cimgui/cimgui.git",
+              "c5a3820d02aeed41e53ba14d8bf94d1c6752293c"),
+
+    DirectorySource("./bundled"),
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+rm cimgui/CMakeLists.txt
+mv imgui wrapper/helper.c wrapper/helper.h wrapper/CMakeLists.txt cimgui/
+mkdir -p cimgui/build && cd cimgui/build
+cmake .. -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release
+make -j${nproc}
+make install
+install_license ../LICENSE ../imgui/LICENSE.txt
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libimgui-cpp", :libimgui),
+    LibraryProduct("libcimgui", :libcimgui),
+    LibraryProduct("libcimgui_helper", :libcimgui_helper),
+    FileProduct("share/compile_commands.json", :compile_commands)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = Dependency[
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
